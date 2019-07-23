da5828601378c9b4235dbb4a54ab5b4a094b2b0b
Dương Mạnh Hoàng <manhhoang3151996@gmail.com>
2019-07-23 13:13:24 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from './footer/footer.component';
+import { ScriptComponent } from './script/script.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeadComponent,
+    FooterComponent,
+    ScriptComponent,
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
